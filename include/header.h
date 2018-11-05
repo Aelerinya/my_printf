4514b31be6503ed2f862d7dc7068155a0a4d2017
Lucie Philippon (Julien) <julien.philippon@epitech.eu>
2018-11-05 14:55:27 +0100

    Add header containing functions prototypes

---
+++ include/header.h
@@ -0,0 +1,16 @@
+/*
+** EPITECH PROJECT, 2018
+** my_printf header
+** File description:
+** Main header for the my_printf project
+*/
+
+#include <stdarg.h>
+
+int my_printf(const char *format, ...);
+int my_putstr(char const *str);
+void my_putchar(char c);
+void print_data(va_list ap, char *str);
+int my_put_nbr(int nb);
+int my_putnbr_base(unsigned int nbr, char const *base);
+int my_showstr(char const *str);
