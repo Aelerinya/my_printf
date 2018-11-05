31c2968009bda604e237ae9e42ea64caa7d82912
Lucie Philippon (Julien) <julien.philippon@epitech.eu>
2018-11-05 15:40:37 +0100

    Unit tests for conversion spcifiers
    Unit tests for every conversion specifiers
    Coverage : 100% of branches and 100% of lines
    Add tests_run directive in Makefile to compile and execute tests

--- Makefile
+++ Makefile
@@ -12,6 +12,8 @@ SRC =	src/my_printf.c \
 	src/my_showstr.c \
 	src/my_putnbr_base.c
 OBJ = $(notdir $(SRC:.c=.o))
+TEST = tests/test_conversion_specifier.c
+TEST_OBJ = $(notdir $(SRC:.c=.o)) $(notdir $(TEST:.c=.o))
 NAME = libmy.a
 
 all: $(NAME)
@@ -27,6 +29,12 @@ clean:
 
 fclean: clean
 	rm -f $(NAME)
+
+tests_run:
+	@gcc -c $(SRC) $(TEST) --coverage -Iinclude
+	@gcc -o tests_run $(TEST_OBJ) --coverage -lcriterion
+	@./tests_run
+	@rm tests_run $(TEST_OBJ)
 
 re: fclean all
 
---
+++ tests/test_conversion_specifier.c
@@ -0,0 +1,29 @@
+/*
+** EPITECH PROJECT, 2018
+** test_conversion_specifiers
+** File description:
+** Tests for the conversion specifiers
+*/
+
+#include <criterion/criterion.h>
+#include <criterion/redirect.h>
+#include "header.h"
+
+void redirect(void)
+{
+    cr_redirect_stdout();
+    cr_redirect_stderr();
+}
+
+Test(conversion_specifiers, normal, .init = redirect)
+{
+    char str[4] = "b\na";
+    char *format = "%s%S%i%i%i%d%b%o%u%x%Xl";
+    char *result = "ab\\012\\177-214748364802147483647-4101112f1Fl";
+    int i = 2147483647;
+    int j = -2147483648;
+
+    str[2] = 127;
+    my_printf(format, "a", str, j, 0, i, -4, 2, 9, 12, 15, 31);
+    cr_assert_stdout_eq_str(result);
+}
