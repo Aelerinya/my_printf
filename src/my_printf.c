da979b542878436878761525cf4b5fe06af6ee49
Lucie Philippon (Julien) <julien.philippon@epitech.eu>
2018-11-05 14:55:34 +0100

    First version of my_printf
    my_printf implements for now these conversion specifiers
      from the C99 specifications of printf :
    s : displays a string
    i and d : displays an int in decimal base
    o : displays an unsigned int in octal
    x, X : displays an unsigned int in hexadecimal,
      in lowercase for x and uppercase for X
    Additional conversion specifiers required by the subject :
    b : displays an unsigned int in binary
    S : displays a string and
      replaces every non-printable character by its octal value

---
+++ src/my_printf.c
@@ -0,0 +1,46 @@
+/*
+** EPITECH PROJECT, 2018
+** my_printf
+** File description:
+** My_printf is an implementation of the printf function
+** according to the C99 standard
+*/
+
+#include <stdarg.h>
+#include "header.h"
+
+int my_printf(const char *format, ...)
+{
+    va_list ap;
+    char *str = (char *)format;
+
+    va_start(ap, format);
+    for (int i = 0; *str != '\0'; i++, str++) {
+        if (*str == '%')
+            print_data(ap, ++str);
+        else
+            my_putchar(*str);
+    }
+    va_end(ap);
+    return (0);
+}
+
+void print_data(va_list ap, char *str)
+{
+    if (*str == 's')
+        my_putstr(va_arg(ap, char *));
+    if (*str == 'S')
+        my_showstr(va_arg(ap, char *));
+    if (*str == 'i' || *str == 'd')
+        my_put_nbr(va_arg(ap, int));
+    if (*str == 'b')
+        my_putnbr_base(va_arg(ap, unsigned int), "01");
+    if (*str == 'o')
+        my_putnbr_base(va_arg(ap, unsigned int), "01234567");
+    if (*str == 'u')
+        my_putnbr_base(va_arg(ap, unsigned int), "0123456789");
+    if (*str == 'x')
+        my_putnbr_base(va_arg(ap, unsigned int), "0123456789abcdef");
+    if (*str == 'X')
+        my_putnbr_base(va_arg(ap, unsigned int), "0123456789ABCDEF");
+}
